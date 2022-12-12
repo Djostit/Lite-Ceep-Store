7c157303923915a402005f1bf11a0950a9186954
Djostit <djostit@mail.ru>
2022-12-12 17:32:23 +0500

    Update #21

---
+++ Lite Ceep Store/Assets/NullableZeroConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lite_Ceep_Store.Assets
+{
+    public class NullableZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return 0;
+            else
+                return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null
+                || value == string.Empty)
+                return 0;
+            else
+                return value;
+        }
+    }
+}
